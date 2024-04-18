ff056626e85bec90075b533b864a7d094f5eb39b
miyeon396 <miyeon396@naver.com>
2024-04-18 23:46:19 +0900

    ch4. member 필드 타입 추가

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,16 +1,38 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //필수
 public class Member {
 
     @Id
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    private LocalDate lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp; //메모리에서만 쓰겠음. 디비와 매핑하지 말 것.
+
 }
