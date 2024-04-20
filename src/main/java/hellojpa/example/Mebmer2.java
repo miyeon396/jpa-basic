045732bf2cb7543a83e1f4d939aea0db96f3fc23
miyeon396 <miyeon396@naver.com>
2024-04-20 14:16:41 +0900

    ch5. 단방향 연관관계

--- src/main/java/hellojpa/example/Mebmer2.java
+++ src/main/java/hellojpa/example/Mebmer2.java
@@ -1,9 +1,8 @@
 package hellojpa.example;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
 public class Mebmer2 {
 
     @Id @GeneratedValue
@@ -13,8 +12,12 @@ public class Mebmer2 {
     @Column(name = "USERNAME")
     private String username;
 
-    @Column(name = "TEAM_ID")
-    private Long teamId;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="TEAM_ID")
+    private Team2 team;
 
     //객체를 테이블에 맞추어 모델링 - 외래키 식별자를 직접 다룸 setTeam이라고 해야할거 같은데 setTeamId를 하게 됨
 
