08050c9c5b10c56dafefcced69a35d1735ddcfdb
miyeon396 <miyeon396@naver.com>
2024-04-20 15:07:46 +0900

    ch5. 양방향 연관관계와 주인 샘플코드
    - 양방향 연관관계시 순수 객체 상태를 고려해서 항상 양쪽에 값을 설정하자.
     - 양쪽 값 설정을 각각 하기보다 연관관계 편의 메소드로 제공하라
     - 팁! 일케 비지니스 로직 들어가는건 set보단 changeTeam과 같이 이름 바꿔서

--- src/main/java/hellojpa/example/Mebmer2.java
+++ src/main/java/hellojpa/example/Mebmer2.java
@@ -17,7 +17,12 @@ public class Mebmer2 {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="TEAM_ID")
-    private Team2 team;
+    private Team2 team; //진짜 매핑 //(테이블에서) 외래키가 있는 쪽이 주인 -> 일케해야 안헷갈림
+
+    public void changeTeam(Team2 team) {
+        this.team = team;
+        team.getMembers().add(this); //양방향 연관관계시 순수 객체 상태를 고려해서 항상 양쪽에 값을 설정하자. 그리고 그걸 각각 하기보다 연관관계 편의 메소드로 제공하라
+    }
 
     //객체를 테이블에 맞추어 모델링 - 외래키 식별자를 직접 다룸 setTeam이라고 해야할거 같은데 setTeamId를 하게 됨
 
--- src/main/java/hellojpa/example/Team2.java
+++ src/main/java/hellojpa/example/Team2.java
@@ -1,11 +1,14 @@
 package hellojpa.example;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Team2 {
 
     @Id
@@ -14,5 +17,8 @@ public class Team2 {
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "team") //나의 반대편 변수 team과 매핑되어있어
+    private List<Mebmer2> members = new ArrayList<>(); //초기화 해놓기. //가짜 매핑 읽기만 하는 것
 
 }
