61e084b003e46776def1a87d2ac947ade0d28899
miyeon396 <miyeon396@naver.com>
2024-04-21 15:04:08 +0900

    ch9. 객체 타입 비교는 equals로 해야함

--- src/main/java/hellojpa/Address2.java
+++ src/main/java/hellojpa/Address2.java
@@ -1,15 +1,13 @@
 package hellojpa;
 
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Embeddable
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Address2 {
 
     private String city;
--- src/main/java/hellojpa/Period2.java
+++ src/main/java/hellojpa/Period2.java
@@ -1,10 +1,7 @@
 package hellojpa;
 
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +9,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Period2 {
 
     private LocalDateTime startDate;
